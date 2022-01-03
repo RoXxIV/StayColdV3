77883fe4b4d0e2632e99e29da78683efc32b1718
evan <evan.hermier@gmail.com>
2022-01-03 12:48:06 +0100

    ajout des middlewares d'authentification

---
+++ middleware/verify-signup.js
@@ -0,0 +1,58 @@
+// import
+const db = require("../models");
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateUsernameOrEmail = (req, res, next) => {
+  // Username
+  User.findOne({
+    username: req.body.username,
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    if (user) {
+      res
+        .status(400)
+        .send({ message: "Echec! Ce Nom d'utilisateur est déja utilisé!" });
+      return;
+    }
+
+    // Email
+    User.findOne({
+      email: req.body.email,
+    }).exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      if (user) {
+        res.status(400).send({ message: "Echec! Cet email est déja utilisé!" });
+        return;
+      }
+      next();
+    });
+  });
+};
+
+checkRolesExisted = (req, res, next) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: `Echec! Le role ${req.body.roles[i]} n'existe pas`,
+        });
+        return;
+      }
+    }
+  }
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateUsernameOrEmail,
+  checkRolesExisted,
+};
+
+module.exports = verifySignUp;
