b18b8b6780b0aaee8b855dbbb90ad20c835ea900
evan <evan.hermier@gmail.com>
2022-01-05 12:59:54 +0100

    installation et import de vue-router@4

---
+++ client/src/router.js
@@ -0,0 +1,11 @@
+import { createWebHistory, createRouter } from "vue-router";
+// component
+
+const routes = [];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
