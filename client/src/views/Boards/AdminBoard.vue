48195f9bca9674fd2844cdde2b6bc75d4d9bb674
evan <evan.hermier@gmail.com>
2022-01-10 17:48:18 +0100

    ajout d'un guard pour la page admin

--- client/src/views/Boards/AdminBoard.vue
+++ client/src/views/Boards/AdminBoard.vue
@@ -6,32 +6,16 @@
 </template>
 
 <script>
-import AccessService from "../../services/access.service";
 import UsersTable from "../../components/Boards/UsersTable.vue";
 export default {
   name: "Admin-board",
   components: { UsersTable },
   data() {
     return {
-      message: "",
-      users: [],
+      message: "Admin Board",
     };
   },
-  methods: {
-    getAdminAccess() {
-      AccessService.getAdminBoard()
-        .then((response) => {
-          // console.log(response);
-          this.message = response.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
-  mounted() {
-    this.getAdminAccess();
-  },
+  methods: {},
 };
 </script>
 
