8208fc10bf08e082cb2017c8926dc8999ec6d957
evan <evan.hermier@gmail.com>
2022-01-13 15:23:43 +0100

    reduction du temp d'animation du router-view

--- client/src/App.vue
+++ client/src/App.vue
@@ -23,10 +23,11 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+/* Transition routeur view __________*/
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s ease;
+  transition: opacity 0.3s ease;
 }
 
 .fade-enter-from,
