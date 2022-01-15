be58920ee99ff317e6be6a141a1806316011faad
evan <evan.hermier@gmail.com>
2022-01-15 20:57:13 +0100

    ajout image formulaire + animation

--- client/src/views/User/EditBath.vue
+++ client/src/views/User/EditBath.vue
@@ -1,6 +1,10 @@
 <template>
   <section>
-    <h1>Modifier une <span class="text-blue">baignade</span></h1>
+    <div>
+      <h1>Modifier une <span class="text-blue">baignade</span></h1>
+      <img src="../../assets/images/poulpe.png" alt="" class="flash" />
+    </div>
+
     <BathForm :editMode="true" />
   </section>
 </template>
@@ -17,14 +21,60 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+/* Section __________*/
 section {
   width: 75%;
   margin: auto;
-  h1 {
-    margin: 50px auto 50px auto;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media (max-width: 611.98px) {
+      flex-direction: column;
+    }
+    h1 {
+      margin: 30px 10px 30px 0;
+    }
+    img {
+      max-width: 60px;
+    }
   }
   @media (max-width: 991.98px) {
     width: 100%;
+  }
+}
+/* Animation poulpe __________*/
+.flash {
+  -webkit-animation-name: flash;
+  animation-name: flash;
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+@-webkit-keyframes flash {
+  0%,
+  50%,
+  100% {
+    opacity: 1;
+  }
+
+  25%,
+  75% {
+    opacity: 0;
+  }
+}
+
+@keyframes flash {
+  0%,
+  50%,
+  100% {
+    opacity: 1;
+  }
+
+  25%,
+  75% {
+    opacity: 0;
   }
 }
 </style>
