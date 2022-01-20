db15d263998068ad00bfcbcf7dd2400415c75452
evan <evan.hermier@gmail.com>
2022-01-20 12:52:17 +0100

    mise en place recuperation du contenu du formulaire de contact + renvoie sur mon mail personnel

---
+++ routes/contact.routes.js
@@ -0,0 +1,35 @@
+/** Routeur express, routes liées au formulaire de contact
+ * @module routes/contact
+ */
+
+/** @requires module:controllers/contact */
+const controller = require("../controllers/contact.controller");
+
+/**
+ * @module routes/contact
+ * @param {object} app
+ */
+module.exports = function (app) {
+  /**
+   * Headers
+   * @param middleware
+   */
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  /**
+   * Envoie le contenu du formulaire de contact sur mon email personnel
+   * @name get/contact
+   * @function
+   * @memberof module:routes/contact
+   * @inner
+   * @param {string} path - Express path
+   * @param {module} controller - controllers/contact.controller.handleFormContact()
+   */
+  app.get("/contact", controller.handleFormContact);
+};
